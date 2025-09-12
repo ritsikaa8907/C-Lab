c6c7a6ea056b981f5d61fcf4f4b90b53e7298565
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2025-09-12 11:56:17 +0530

    Create Question 26 .c

---
+++ Question 26 .c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+    return 0;
+}
+Write a program to print numbers from 1 to n.
+
