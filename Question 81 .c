bc0d24ded1f8dae1ddb974ff3b5b218cf9cae671
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2025-10-02 21:44:52 +0530

    Create Question 81 .c

---
+++ Question 81 .c
@@ -0,0 +1,20 @@
+//Count characters in a string without using built-in length functions.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0, i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0') {
+        count++;
+        i++;
+    }
+
+    printf("Number of characters in the string: %d\n", count);
+
+    return 0;
+}
