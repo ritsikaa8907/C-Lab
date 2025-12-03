cf78548e3edede153d2b40c99632fb676821c071
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2025-12-03 12:14:23 +0530

    Update Question 57 .c

--- Question 57 .c
+++ Question 57 .c
@@ -5,20 +5,17 @@
 int main() {
     int n, i, sum = 0;
     
-    // Ask user for number of elements
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
 
-    int arr[n];  // declare array of size n
+    int arr[n];  
 
-    // Input array elements
     printf("Enter %d elements:\n", n);
     for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
-        sum += arr[i];  // add each element to sum
+        sum += arr[i]; 
     }
 
-    // Print sum
     printf("Sum of array elements = %d\n", sum);
 
     return 0;
